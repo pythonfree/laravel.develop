2160ff36243256ed21a67aa783bc306ebc13cccb
pseex <evgeniyshakhov@gmail.com>
2021-07-13 19:11:10 +0300

    Создали меню и контроллеры

---
+++ resources/views/menu.php
@@ -0,0 +1,3 @@
+<a href="<?=route('Home')?>">Главная</a>
+<a href="<?=route('Admin', null, false)?>">Админка</a>
+<br>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,32 @@
 |
 */
 
-Route::get('/', function () {
+
+
+
+//Route::get('/', 'HomeController@index');
+
+Route::get('/', 'HomeController@index')->name('Home');
+/*Route::get('/',
+    [
+        'uses' => 'HomeController@index',
+        'as' => 'Home'
+    ]
+);*/
+
+Route::get('/admin', 'Admin\IndexController@index')->name('Admin');
+//Route::get('/admin',
+//    [
+//        'uses' => 'Admin\IndexController@index',
+//        'as' => 'Admin'
+//    ]
+//);
+
+
+
+/*Route::get('/', function () {
 //    return view('welcome');
-    return 'Hello World!!!!';
-});
+//    return 'Hello World!!!!';
+//    info('1111');
+    return view('index');
+});*/
