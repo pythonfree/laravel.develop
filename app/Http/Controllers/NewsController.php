813e5e71fb5d5a69094f90c3cba22342a47b8bc7
pseex <evgeniyshakhov@gmail.com>
2021-08-04 21:43:46 +0300

    Отказались от сырых запросов к таблице, однако, запросы по прежнему в контроллере

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,17 +10,19 @@ class NewsController extends Controller
 {
     public function index()
     {
-        $news = DB::select('SELECT * FROM news');
+        //$news = DB::select('SELECT * FROM news');
+        $news = DB::table('news')->get();
         //dd($news);
         return view('news.index')->with('news', $news);
     }
 
     public function show($id)
     {
-        $article = DB::select('SELECT * FROM news WHERE id = :id', ['id' => $id]);
+        //$article = DB::select('SELECT * FROM news WHERE id = :id', ['id' => $id]);
+        $article = DB::table('news')->find($id);
         //dd($article);
         if (!empty($article)) {
-            return view('news.One')->with('article', $article[0]);
+            return view('news.One')->with('article', $article);
         } else {
             return redirect()->route('news.index');
         }
